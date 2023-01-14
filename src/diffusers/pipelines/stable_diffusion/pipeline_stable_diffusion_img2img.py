e20b28f81f3fd34f421e519036081e92b79829c3
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2023-01-14 12:55:12 -0400

    Update pipeline_stable_diffusion_img2img.py

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
@@ -680,6 +680,8 @@ class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
         extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
 
         # 8. Denoising loop
+        initial_pred_set = False
+        count = 0
         blocks = [0,1,2,3]
         LGP = latent_guidance_predictor(output_dim=4, input_dim=7080, num_encodings=9).to(device)
         checkpoint = torch.load(LGP_path, map_location=device)
@@ -688,6 +690,7 @@ class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
         num_warmup_steps = len(timesteps) - num_inference_steps * self.scheduler.order
         with self.progress_bar(total=num_inference_steps) as progress_bar:
             for i, t in enumerate(timesteps):
+                count+=1
                 # expand the latents if we are doing classifier free guidance
                 latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
                 latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
@@ -724,15 +727,19 @@ class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
                 pred_edge_map = LGP(features, noise_lvl)
                 pred_edge_map = pred_edge_map.unflatten(0, (1, 64, 64))
                 pred_edge_map = pred_edge_map.transpose(3, 1)
-                gradient = grad(pred_edge_map, target_latent)
-
+                
+                if not initial_pred_set:
+                    initial_pred = pred_edge_map
+                    initial_pred_set = True
+                
                 # compute the previous noisy sample x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
                 
-                alpha = (torch.linalg.vector_norm(latent_model_input[:1] - latents))/(torch.linalg.vector_norm(gradient))
-                alpha = alpha * edge_guidance_scale
-                
-                latents = latents - alpha * gradient
+                if count > 2:
+                    gradient = grad(pred_edge_map, initial_pred)
+                    alpha = (torch.linalg.vector_norm(latent_model_input[:1] - latents))/(torch.linalg.vector_norm(gradient))
+                    alpha = alpha * edge_guidance_scale                
+                    latents = latents - alpha * gradient
 
                 # call the callback, if provided
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
