1836f87feffe24cb363d5abc401f36a6ac26bc5b
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2023-01-19 19:36:51 -0400

    Update pipeline_stable_diffusion_img2img.py

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
@@ -532,10 +532,10 @@ class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
     def __call__(
         self,
         prompt: Union[str, List[str]],
-        height: Optional[int] = None,
-        width: Optional[int] = None,
         LGP_path: str,
         edge_guidance_scale: float,
+        height: Optional[int] = None,
+        width: Optional[int] = None,
         image: Union[torch.FloatTensor, PIL.Image.Image] = None,
         num_inference_steps: Optional[int] = 50,
         guidance_scale: Optional[float] = 7.5,
